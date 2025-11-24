7de39b8851ac2c1c3dc25e33a7389529ec826aad
⠀Sonomaka ⠀ <joaogalvero@id.uff.br>
2025-11-24 00:23:37 -0300

    Adicionando pastas do back

---
+++ PHP/connection.php
@@ -0,0 +1,17 @@
+<?php 
+function db()
+{
+    static $pdo = null;
+
+    if ($pdo === null) {
+        $dsn = "pgsql:host=localhost;port=5432;dbname=devweb_project;";
+        $user = "devweb_project";
+        $pass = "";
+
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
+
+    return $pdo;
+}
---
+++ users/view.php
@@ -0,0 +1,39 @@
+<?php
+
+function login_user($request, $pdo) {
+	session_start();
+	$stmt = $pdo->prepare("
+        SELECT id, email, password_hash, name
+        FROM app.users
+        WHERE email = :email
+        LIMIT 1
+    ");
+
+    $stmt->execute([':email' => $request['email']]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$user || !password_verify($request['password'], $user['password_hash'])) {
+		$_SESSION['error'] = "Email ou senha inválidos.";
+		return header("Location: template/login.html");
+	}
+
+    $_SESSION['user_id']    = $user['id'];
+    $_SESSION['user_email'] = $user['email'];
+    $_SESSION['user_name']  = $user['name'];
+	return header("Location: ../../index.html");
+}
+
+function signup_user($request, $pdo) {
+    $hash = password_hash($request['password'], PASSWORD_DEFAULT);
+
+    $stmt = $pdo->prepare("
+        INSERT INTO app.users (email, password_hash, name)
+        VALUES (:email, :password_hash, :name)
+    ");
+
+    $stmt->execute([
+        ':email' => $request['email'],
+        ':password_hash' => $hash,
+        ':name' => $request['name']
+    ]);
+}
