0e7c4e166c35b844b4c76894f9f001babccf8121
⠀Sonomaka ⠀ <joaogalvero@id.uff.br>
2025-11-27 04:31:10 -0300

    Quem-somos semi-pronto

--- quem_somos.php
+++ quem_somos.php
@@ -20,9 +20,16 @@
 
     <main>
         <div class="media-player-wrapper">
-            <button class="btn-voltar"></button>
+                <button class="btn-voltar"></button>
+
+            <section>
+                <h1>A <strong id="Audio-Angel">Audio-Angel Library.01</strong> é uma plataforma criada para quem deseja organizar suas playlists de um jeito simples e prático.</h2>
+                <h2>Aqui, você reune suas músicas favoritas e organiza do jeito que achar melhor — ordenar, categorizar e guardar o que mais combina com você!</p>
+                <p>O site funciona como uma biblioteca de playlists, <br>oferecendo um espaço leve e focado na curadoria individual, onde cada usuário constrói seu próprio acervo sonoro.</p>
+            </section>
         </div>
     </main>
+
     <footer class="footer">
         <p>© 2025 AUDIO·ANG3L LIBRARY·01 — CRIADO POR PEDRO GALVERO E KAUAI TÁVORA APENAS PARA USO EDUCATIVO — AAL01</p>
     </footer>
